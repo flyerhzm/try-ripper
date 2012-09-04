5e073ecf792d5e27686f9c4ef65c359fc9ccb55c
Richard Huang <flyerhzm@gmail.com>
2012-09-04 23:33:31 +0800

    better style

--- app.rb
+++ app.rb
@@ -11,7 +11,37 @@ get '/' do
 end
 
 post '/' do
-  code = params["code"]
-  @sexp = Ripper::SexpBuilder.new(code).parse
+  @code = params["code"]
+  sexp = Ripper::SexpBuilder.new(@code).parse
+  @result = ""
+  @indent = -1
+  parse(sexp)
   haml :index
+end
+
+def parse(nodes)
+  @indent += 1
+  nodes.each do |node|
+    if node.is_a? Array
+      parse(node)
+    else
+      @result << "&nbsp;" * @indent * 2
+      @result << output(node)
+      @result << "<br/>"
+    end
+  end
+  @indent -= 1
+end
+
+def output(node)
+  case node
+  when NilClass
+    "nil"
+  when Symbol
+    "<span class='red'>:#{node.to_s}</span>"
+  when Fixnum
+    "<span class='blue'>#{node.to_s}</span>"
+  else
+    node.inspect
+  end
 end
