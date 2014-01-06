b47fe2eb636c7390563cfaf36f02be4af42aa30c
Richard Huang <flyerhzm@gmail.com>
2014-01-06 23:31:07 +0800

    use bower to manage js/css

--- static/lib/app.js
+++ static/lib/app.js
@@ -1,4 +1,10 @@
 $(function() {
+  $('.nav-tabs a').click(function() {
+    $('.nav-tabs li').removeClass('active');
+    $(this).parent().addClass('active');
+    $('input[name=source]').val($(this).attr('href').replace('#', ''));
+  });
+
   $('pre div').addClass("unfold");
   $('pre div').click(function() {
     var $self = $(this);
